6fe29e480cd46e9d9ce70425c62ea74936b6f37e
Roman Ryu <ryuroman@himnerom.com>
2023-04-10 17:36:08 +0900

    App setup & Assets

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple calculator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
