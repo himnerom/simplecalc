435d18ba2ae88ef230651a24a936b30396e99770
Rom <ryuroman@himnerom.com>
2026-09-06 22:23:01 +0200

    Fix themedialog layout issues
    + add shape interaction

--- lib/services/screen_service.dart
+++ lib/services/screen_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ScreenService {
@@ -11,7 +12,7 @@ class ScreenService {
 
   /// Returns true if the current platform is a desktop
   static bool get isDesktop =>
-      Platform.isMacOS || Platform.isLinux || Platform.isWindows;
+      !kIsWeb && (Platform.isMacOS || Platform.isLinux || Platform.isWindows);
 
   /// Returns the padding surrounding the calc layout
   double get layoutPadding => layoutSize * 0.075;
--- lib/widgets/calc_drawer_theme.dart
+++ lib/widgets/calc_drawer_theme.dart
@@ -3,157 +3,214 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 
 import 'package:simple_calculator/services/screen_service.dart';
+import 'package:simple_calculator/services/shape_service.dart';
 import 'package:simple_calculator/services/themes_service.dart';
 
 class CalcDrawerTheme extends StatelessWidget {
   const CalcDrawerTheme({super.key});
 
+  static const double maxWidth = 280;
+
   @override
   Widget build(BuildContext context) {
     final theme = context.watch<ThemesService>().theme;
+    final double borderRadius = context.watch<ShapeService>().borderRadius;
     final screen = ScreenService(context);
+    final double factor = screen.width < (maxWidth + 80)
+        ? screen.width / (maxWidth + 80)
+        : 1;
+
+    final double dialogWidth = maxWidth * factor;
 
     return Dialog(
       clipBehavior: Clip.antiAlias,
-      backgroundColor: theme.resBackgroundColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-        side: BorderSide(color: theme.btn2BackgroundColor, width: 6),
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.symmetric(
+        horizontal: 40 * factor,
+        vertical: 24 * factor,
       ),
-      child: Container(
-        height: screen.height * 0.45,
-        constraints: BoxConstraints(maxWidth: 280),
-        child: Column(
-          children: [
-            Container(
-              color: theme.btn2BackgroundColor,
-              height: screen.height * 0.025,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: ThemesService.themesList.asMap().entries.map((
-                    entry,
-                  ) {
-                    final int idx = entry.key;
-                    final ThemePalette tp = entry.value;
+      constraints: BoxConstraints(maxWidth: dialogWidth),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(borderRadius * factor),
+      ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final double contentFactor = constraints.maxWidth < maxWidth
+              ? constraints.maxWidth / maxWidth
+              : 1;
 
-                    return Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        /// Divider between categories
-                        if (idx > 0 &&
-                            ThemesService.themesList[idx - 1].themeCategory !=
-                                tp.themeCategory)
-                          Container(
-                            height: 3,
-                            width: double.infinity,
-                            color: theme.btn2BackgroundColor,
-                          ),
-
-                        Slidable(
-                          endActionPane:
-                              tp.themeCategory != ThemeCategory.simple &&
-                                  tp.themeCategory != ThemeCategory.double
-                              ? null
-                              : ActionPane(
-                                  motion: const ScrollMotion(),
-                                  children: [
-                                    SlidableAction(
-                                      onPressed: (_) => context
-                                          .read<ThemesService>()
-                                          .setTheme(idx, Brightness.light),
-                                      backgroundColor:
-                                          ThemesService.lightBackground,
-                                      foregroundColor: tp.primaryColor,
-                                      icon: Icons.circle,
-                                    ),
-                                    SlidableAction(
-                                      onPressed: (_) => context
-                                          .read<ThemesService>()
-                                          .setTheme(idx, Brightness.dark),
-                                      backgroundColor:
-                                          ThemesService.darkBackground,
-                                      foregroundColor: tp.primaryColor,
-                                      icon: Icons.circle,
-                                    ),
-                                  ],
-                                ),
-                          child: InkWell(
-                            highlightColor: tp.primaryColor.withOpacity(0.1),
-                            splashColor: tp.primaryColor.withOpacity(0.05),
-                            onTap: () =>
-                                context.read<ThemesService>().setTheme(idx),
-                            child: Container(
-                              color: theme.themeName == tp.themeName
-                                  ? tp.primaryColor.withOpacity(0.05)
-                                  : Colors.transparent,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 24,
-                                vertical: 12,
-                              ),
-                              child: Row(
-                                children: [
-                                  Card(
-                                    color: theme.primaryColor,
-                                    shadowColor: Colors.transparent,
-                                    clipBehavior: Clip.antiAlias,
-                                    child: SizedBox(
-                                      height: 20,
-                                      width: 20,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          gradient:
-                                              tp.themeCategory ==
-                                                  ThemeCategory.special
-                                              ? const SweepGradient(
-                                                  colors: ThemesService
-                                                      .rainbowColors,
-                                                )
-                                              : LinearGradient(
-                                                  begin: Alignment.topRight,
-                                                  end: Alignment.bottomLeft,
-                                                  colors: [
-                                                    tp.secondaryColor,
-                                                    tp.secondaryColor,
-                                                    tp.secondaryColor,
-                                                    tp.primaryColor,
-                                                    tp.primaryColor,
-                                                    tp.primaryColor,
-                                                  ],
-                                                ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 12),
-                                  Expanded(
-                                    child: Text(
-                                      tp.themeName,
-                                      style: TextStyle(
-                                        color: theme.resTextColor,
-                                        fontSize: 21,
-                                        fontWeight: FontWeight.w800,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  }).toList(),
-                ),
+          return Card(
+            color: theme.resBackgroundColor,
+            elevation: 0,
+            margin: EdgeInsets.zero,
+            clipBehavior: Clip.antiAlias,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(borderRadius * factor),
+              side: BorderSide(
+                color: theme.btn2BackgroundColor,
+                width: 6 * contentFactor,
               ),
             ),
-            Container(
-              color: theme.btn2BackgroundColor,
-              height: screen.height * 0.025,
+            child: SizedBox(
+              width: double.infinity,
+              height: screen.height * 0.45,
+              child: Column(
+                children: [
+                  Container(
+                    height: screen.height * 0.025 * contentFactor,
+                    width: double.infinity,
+                    color: theme.btn2BackgroundColor,
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: ThemesService.themesList.asMap().entries.map((
+                          entry,
+                        ) {
+                          final int idx = entry.key;
+                          final ThemePalette tp = entry.value;
+
+                          return Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              /// Divider between categories
+                              if (idx > 0 &&
+                                  ThemesService
+                                          .themesList[idx - 1]
+                                          .themeCategory !=
+                                      tp.themeCategory)
+                                Container(
+                                  height: 3 * contentFactor,
+                                  width: double.infinity,
+                                  color: theme.btn2BackgroundColor,
+                                ),
+
+                              SizedBox(
+                                width: double.infinity,
+                                child: Slidable(
+                                  endActionPane:
+                                      tp.themeCategory !=
+                                              ThemeCategory.simple &&
+                                          tp.themeCategory !=
+                                              ThemeCategory.double
+                                      ? null
+                                      : ActionPane(
+                                          motion: const ScrollMotion(),
+                                          children: [
+                                            SlidableAction(
+                                              onPressed: (_) => context
+                                                  .read<ThemesService>()
+                                                  .setTheme(
+                                                    idx,
+                                                    Brightness.light,
+                                                  ),
+                                              backgroundColor:
+                                                  ThemesService.lightBackground,
+                                              foregroundColor: tp.primaryColor,
+                                              icon: Icons.circle,
+                                            ),
+                                            SlidableAction(
+                                              onPressed: (_) => context
+                                                  .read<ThemesService>()
+                                                  .setTheme(
+                                                    idx,
+                                                    Brightness.dark,
+                                                  ),
+                                              backgroundColor:
+                                                  ThemesService.darkBackground,
+                                              foregroundColor: tp.primaryColor,
+                                              icon: Icons.circle,
+                                            ),
+                                          ],
+                                        ),
+                                  child: InkWell(
+                                    highlightColor: tp.primaryColor.withOpacity(
+                                      0.1,
+                                    ),
+                                    splashColor: tp.primaryColor.withOpacity(
+                                      0.05,
+                                    ),
+                                    onTap: () => context
+                                        .read<ThemesService>()
+                                        .setTheme(idx),
+                                    child: Container(
+                                      color: theme.themeName == tp.themeName
+                                          ? tp.primaryColor.withOpacity(0.05)
+                                          : Colors.transparent,
+                                      padding: EdgeInsets.symmetric(
+                                        horizontal: 24 * contentFactor,
+                                        vertical: 12 * contentFactor,
+                                      ),
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            height: 20 * contentFactor,
+                                            width: 20 * contentFactor,
+                                            decoration: BoxDecoration(
+                                              gradient:
+                                                  tp.themeCategory ==
+                                                      ThemeCategory.special
+                                                  ? const SweepGradient(
+                                                      colors: ThemesService
+                                                          .rainbowColors,
+                                                    )
+                                                  : LinearGradient(
+                                                      begin: Alignment.topRight,
+                                                      end: Alignment.bottomLeft,
+                                                      colors: [
+                                                        tp.secondaryColor,
+                                                        tp.secondaryColor,
+                                                        tp.secondaryColor,
+                                                        tp.primaryColor,
+                                                        tp.primaryColor,
+                                                        tp.primaryColor,
+                                                      ],
+                                                    ),
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                    borderRadius /
+                                                        2 *
+                                                        contentFactor,
+                                                  ),
+                                            ),
+                                          ),
+                                          SizedBox(width: 12 * contentFactor),
+                                          Expanded(
+                                            child: Text(
+                                              tp.themeName,
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: TextStyle(
+                                                color: theme.resTextColor,
+                                                fontSize: 18 * contentFactor,
+                                                fontWeight: FontWeight.w800,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: screen.height * 0.025 * contentFactor,
+                    width: double.infinity,
+                    color: theme.btn2BackgroundColor,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
